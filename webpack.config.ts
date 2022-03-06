963b258ef7526eef7e9abf4f1c9d0cd588f67570
Marta Fret <1marta.fret@gmail.com>
2022-03-06 14:55:26 +0100

    initial setup

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import path from 'path';
+import * as webpack from 'webpack';
+import WebpackDevServer from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  context: path.resolve(__dirname),
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+
+  devServer: {
+    static: './dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
+
+export default config;
